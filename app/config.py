7c56b6d6d4624e860980e1d95450c06e98213cf2
Rakesh K <rakeshkraki3272@gmail.com>
2026-02-25 23:21:04 +0530

    Validation

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseSettings
 
-
+#Base class type is unknown, obscuring type of derived class
 class Settings(BaseSettings):
     app_name: str = "Type Safe API"
     debug: bool = False
