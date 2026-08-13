11d1b710e08b5a7a84c104f068946fd0f7fc0348
Rakesh K <rakeshkraki3272@gmail.com>
2026-08-13 22:44:48 +0530

    pydantic Validation2

--- my_app/pydantic_usecase.py
+++ my_app/pydantic_usecase.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel, dataclasses
+from pydantic import BaseModel, dataclasses, Field
 
 
 class Agent(BaseModel):
-    id: str
+    id: str = Field(
+        ...,
+        min_length=4,
+        max_length=6
+    )
     name: str
     description: str
     role: str
 
-input_data = {"id":"1", "name":"HR Policy", "description":"validates", "role": "take decisions"}
+input_data = {"id":"11111111111111", "name":"HR Policy", "description":"validates", "role": "take decisions"}
 
 agent_model = Agent(**input_data)
 
